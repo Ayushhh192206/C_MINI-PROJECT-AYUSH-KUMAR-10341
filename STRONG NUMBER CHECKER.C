87eda4bbd8a135865720b9b28de92084db845e0a
Aayush <ayushray058@gmail.com>
2025-12-30 15:02:17 +0530

    mini

---
+++ STRONG NUMBER CHECKER.C
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(){
+    int num,temp,digit;
+    int count = 0;
+    int sum = 0;
+    int power,i;
+
+    printf("Enter a number: ");
+    scanf("%d",&num);
+
+   
+    temp=num;
+    while(temp!= 0) {
+        count++;
+        temp =temp /10;
+    }
+
+   
+    temp=num;
+    while(temp!= 0) {
+        digit =temp %10;
+
+        power =1;
+        for(i =1;i <=count;i++) {
+            power =power*digit;
+        }
+
+        sum = sum+power;
+        temp = temp/10;
+    }
+
+    if (sum ==num)
+        printf("%d is an Armstrong number\n",num);
+    else
+        printf("%d is not an Armstrong number\n",num);
+
+    return 0;
+}
